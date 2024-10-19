ab90ba306f062b85465d1c9ac0c3c37c2b9cd940
kgiannopoulou <167083518+kgiannopoulou@users.noreply.github.com>
2024-10-19 20:08:00 +0300

    questions.js

---
+++ questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What type of story are you in the mood for?",
+        options: [
+            "a) Heartwarming romance",
+            "b) Intense suspense and thrill",
+            "c) Fantasy with magical elements",
+            "d) Light-hearted comedy"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How long are you willing to watch?",
+        options: [
+            "a) A short series (12-16 episodes)",
+            "b) A full series (20+ episodes)",
+            "c) Just a movie or special episode"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which setting excites you the most?",
+        options: [
+            "a) High school or university life",
+            "b) Corporate/business world",
+            "c) Historical period drama",
+            "d) Fantasy world or alternate reality"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What kind of lead characters do you prefer?",
+        options: [
+            "a) Strong and independent",
+            "b) Soft and nurturing",
+            "c) Quirky and unpredictable",
+            "d) Mysterious and reserved"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How much emotional drama can you handle?",
+        options: [
+            "a) Give me all the feels! I love tear-jerkers.",
+            "b) Some drama, but with a happy ending.",
+            "c) I prefer mostly light-hearted moments.",
+            "d) Minimal emotions, just action and plot twists."
+        ]
+    }
+];
+
