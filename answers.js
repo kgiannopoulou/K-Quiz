b5b4c28a3a96f7783e1c2bb5ee731b0058477d67
kgiannopoulou <167083518+kgiannopoulou@users.noreply.github.com>
2024-10-19 20:08:20 +0300

    answers.js

---
+++ answers.js
@@ -0,0 +1,8 @@
+// answers.js
+
+let userAnswers = [];  // Array to store user responses
+
+// Function to store answer for each question
+function storeAnswer(questionIndex, answerIndex) {
+    userAnswers[questionIndex] = answerIndex;
+}
